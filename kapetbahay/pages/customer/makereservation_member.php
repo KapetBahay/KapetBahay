b73d0f777f6be34d0c8f12d01f6b5bd33ac80518
KapetBahay <kapetbahay2024@gmail.com>
2024-07-05 15:38:42 +0800

    Update makereservation_member.php

--- kapetbahay/pages/customer/makereservation_member.php
+++ kapetbahay/pages/customer/makereservation_member.php
@@ -140,7 +140,7 @@ mysqli_close($con);
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="table">Table Number</label>
+                            <label for="table">Table for How Many?</label>
                             <select class="form-control" id="table" name="table" required>
                                 <?php
                                 for ($i = 1; $i <= 15; $i++) {
